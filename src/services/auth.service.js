9097e7f8b8082b20ac845722aae97c62f983900f
Oleksandr Kravchuk <o.m.kravchuk19@gmail.com>
2021-10-06 16:37:15 +0300

    change serve

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "";
 
 const register = (username, age, email, password) => {
   return axios.post(API_URL + "/api/auth/register", {
