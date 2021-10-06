99d63ae104a0fcab70d8d18cc09bc9fa57165307
Oleksandr Kravchuk <o.m.kravchuk19@gmail.com>
2021-10-06 17:51:43 +0300

    change serve

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ const {
 // app.get('/*', function (req, res) {
 //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 // });
-app.use(express.static(__dirname + '/build'));
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/build/index.html'));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 const optionsCors = {
   'origin': '*',
